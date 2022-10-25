729733aa05325ef6b8c76766172a3357f0505af7
icampagnolo <102811925+icampagnolo@users.noreply.github.com>
2022-10-25 21:57:41 +0200

    Update gestione.py

--- gestione.py
+++ gestione.py
@@ -7,7 +7,7 @@ import os
 from flask import Flask,render_template,redirect, request
 import json
 
-
+port=os.environ.get("PORT",5000)
 
 app = Flask(__name__)            
 
@@ -40,6 +40,5 @@ def index():
                         return render_template(pagina)
 
 
-#if __name__ == '__main__':
-port=os.environ.get("PORT",5000)
-app.run(debug=False, host="0.0.0.0", port=port) 
+if __name__ == '__main__':
+         app.run(debug=False, host="0.0.0.0", port=port) 
